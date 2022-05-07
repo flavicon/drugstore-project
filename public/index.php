954e29ba398d2066412f64b47c859c4a5be755a8
flavio silva <flavio.devjs@gmail.com>
2022-05-07 13:47:33 -0300

    initial commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+ini_set('display_errors', 1);
+
+$url = $_SERVER['REQUEST_URI'];
---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class ErrorController
+{
+    public function notFound(): void
+    {
+        echo 'Página não encontrada';
+    }
+}
